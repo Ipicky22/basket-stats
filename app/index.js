fd0da9070274ba51dbba3f39e6bd89700d2edf79
Adrien Masson <adrien.masson@hotmail.fr>
2023-03-19 12:45:33 +0100

    zustand store

--- app/index.js
+++ app/index.js
@@ -1,13 +1,41 @@
-import { View, Text } from "react-native";
+import { Button, View, Text } from "react-native";
 import { Stack } from "expo-router";
+import { useReset, useStore } from "../store/globalStore";
 
 const Home = () => {
+	const { items, addItem } = useStore(({ addItem, items }) => ({
+		items,
+		addItem,
+	}));
+
 	return (
-		<View>
-			<Stack.Screen options={{ title: "Historique" }} />
-			<Text>Home</Text>
+		<View style={{ flex: 1, padding: 12 }}>
+			<Stack.Screen options={{ title: "Matchs" }} />
+			<TodoList />
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "space-around",
+				}}>
+				<Button
+					onPress={() => addItem(`Item #${items.length}`)}
+					title='Add Item'
+				/>
+				<Button onPress={() => useReset()} title='reset' />
+			</View>
 		</View>
 	);
 };
+
+function TodoList() {
+	const items = useStore(({ items }) => items);
+	return (
+		<View style={{ flex: 1 }}>
+			{items.map((item) => (
+				<Text key={item.id}>{item.text}</Text>
+			))}
+		</View>
+	);
+}
 
 export default Home;
