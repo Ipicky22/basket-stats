c266133621d881af6a8a2a9a4c7ac02e77a0883f
Adrien Masson <adrien.masson@hotmail.fr>
2023-03-25 10:47:03 +0100

    add stats to player

--- app/stats.js
+++ app/stats.js
@@ -2,73 +2,67 @@ import { useEffect, useState } from "react";
 import { View, Text, Pressable, FlatList, Button } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import { globalStore } from "../store/globalStore";
-import { Stopwatch } from "react-native-stopwatch-timer";
-
-const AddPoints = () => {
-	const addPointsTab = ["+1", "+2", "+3"];
-	return (
-		<View style={{ flexDirection: "row", justifyContent: "space-around" }}>
-			{addPointsTab.map((item, index) => {
-				return (
-					<View
-						style={{
-							borderColor: "black",
-							borderStyle: "solid",
-							borderWidth: 1,
-							borderRadius: 5,
-							width: 50,
-							height: 50,
-							justifyContent: "center",
-							alignItems: "center",
-							margin: 4,
-						}}
-						key={"add" + (index + 1)}>
-						<Text>{item}</Text>
-					</View>
-				);
-			})}
-		</View>
-	);
-};
-
-const FailedPoints = () => {
-	const failedPointsTab = ["-1", "-2", "-3"];
-	return (
-		<View style={{ flexDirection: "row", justifyContent: "space-around" }}>
-			{failedPointsTab.map((item, index) => {
-				return (
-					<View
-						style={{
-							borderColor: "black",
-							borderStyle: "solid",
-							borderWidth: 1,
-							borderRadius: 5,
-							width: 50,
-							height: 50,
-							justifyContent: "center",
-							alignItems: "center",
-							margin: 4,
-						}}
-						key={"failed" + (index + 1)}>
-						<Text>{item}</Text>
-					</View>
-				);
-			})}
-		</View>
-	);
-};
+import Stopwatch from "./components/stopwatch";
 
 const Stats = () => {
 	const { currentMatch, currentQuart } = globalStore();
 	const [startTimer, setStartTimer] = useState(false);
 	const [selectedTeam, setSelectedTeam] = useState([]);
+	const [currentTime, setCurrentTime] = useState();
 
-	const options = {
-		text: {
-			fontSize: 30,
-			color: "#000",
-			marginLeft: 7,
-		},
+	const [statsArray, setStatsArray] = useState([
+		"+1",
+		"+2",
+		"+3",
+		"-1",
+		"-2",
+		"-3",
+		"PD",
+	]);
+	const [selectedStat, setSelectedStat] = useState("");
+	const [selectedStatPlayer, setSelectedStatPlayer] = useState("");
+	const [statsPlayers, setStatsPlayers] = useState([]);
+
+	useEffect(() => {
+		let tmpArray = [];
+		currentMatch.team.map((player) => {
+			let objPlayer = {
+				id: player,
+				"+1": 0,
+				"+2": 0,
+				"+3": 0,
+				"-1": 0,
+				"-2": 0,
+				"-3": 0,
+				PD: 0,
+			};
+			tmpArray.push(objPlayer);
+		});
+		setStatsPlayers(...statsPlayers, tmpArray);
+	}, []);
+
+	console.log("statsPlayers => ", statsPlayers);
+	console.log("selectedStat => ", selectedStat);
+	console.log("selectedStatPlayer => ", selectedStatPlayer);
+
+	const addStat = () => {
+		let fixStatsPlayers = statsPlayers;
+		setStatsPlayers(
+			fixStatsPlayers.map((player) => {
+				if (selectedStatPlayer == player.id) {
+					prevValue = player[selectedStat];
+					console.log("prevValue => ", prevValue);
+					return {
+						...player,
+						[selectedStat]: prevValue + 1,
+					};
+				} else {
+					return player;
+				}
+			})
+		);
+		setSelectedStat("");
+		setSelectedStatPlayer("");
 	};
 
 	const selectedPlayer = (number) => {
@@ -134,37 +128,103 @@ const Stats = () => {
 							);
 						})}
 					</View>
-					<View style={{ marginTop: 8, alignItems: "center" }}>
-						<Stopwatch
-							start={startTimer}
-							reset={false}
-							options={options}
-							msecs
-						/>
-					</View>
-					<View
-						style={{
-							flexDirection: "row",
-							alignItems: "center",
-							justifyContent: "space-around",
-							marginTop: 16,
-						}}>
-						<Button
-							title='Play'
-							disabled={selectedTeam.length < 5 ? true : false}
-							onPress={() => setStartTimer(true)}
-						/>
-						<Button
-							title='Stop'
-							onPress={() => setStartTimer(false)}
-						/>
-					</View>
+					<Stopwatch />
 				</View>
 
-				<View style={{ marginTop: 16 }}>
-					<Text style={{ fontSize: 24 }}>Statistiques</Text>
-					<AddPoints />
-					<FailedPoints />
+				<Text style={{ fontSize: 24, marginTop: 16 }}>
+					Statistiques
+				</Text>
+				<View
+					style={{
+						marginTop: 16,
+						flexDirection: "row",
+						flexWrap: "wrap",
+					}}>
+					{statsArray.map((item, index) => {
+						return (
+							<Pressable onPress={() => setSelectedStat(item)}>
+								<View
+									style={{
+										borderColor: "black",
+										borderStyle: "solid",
+										borderWidth: 1,
+										borderRadius: 5,
+										width: 50,
+										height: 50,
+										justifyContent: "center",
+										alignItems: "center",
+										margin: 4,
+										backgroundColor:
+											selectedStat == item
+												? "green"
+												: null,
+									}}
+									key={item}>
+									<Text>{item}</Text>
+								</View>
+							</Pressable>
+						);
+					})}
+				</View>
+
+				<Text style={{ fontSize: 24, marginTop: 16 }}>
+					Assign to Player
+				</Text>
+				<View
+					style={{
+						marginTop: 16,
+						flexDirection: "row",
+						flexWrap: "wrap",
+					}}>
+					{selectedTeam.map((item, index) => {
+						return (
+							<Pressable
+								onPress={() => setSelectedStatPlayer(item)}>
+								<View
+									style={{
+										borderColor: "black",
+										borderStyle: "solid",
+										borderWidth: 1,
+										borderRadius: 5,
+										width: 50,
+										height: 50,
+										justifyContent: "center",
+										alignItems: "center",
+										margin: 4,
+										backgroundColor:
+											selectedStatPlayer == item
+												? "green"
+												: null,
+									}}
+									key={item}>
+									<Text>{item}</Text>
+								</View>
+							</Pressable>
+						);
+					})}
+				</View>
+				<View
+					style={{
+						marginTop: 16,
+						flexDirection: "row",
+						justifyContent: "space-around",
+					}}>
+					<Button
+						title='Reset Selected Stat'
+						onPress={() => {
+							setSelectedStat("");
+							setSelectedStatPlayer("");
+						}}
+					/>
+					<Button
+						title='Add Stat'
+						disabled={
+							selectedStat !== "" && selectedStatPlayer !== ""
+								? false
+								: true
+						}
+						onPress={() => addStat()}
+					/>
 				</View>
 			</View>
 		</View>
