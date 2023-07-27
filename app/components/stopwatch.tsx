35c8f55e52f2c7fbfe1cecc9b3f2169c2b5cec43
Adrien Masson <adrien.masson@hotmail.fr>
2023-07-27 17:59:31 +0200

    stopwatch component TS and fix refresh rate

--- app/components/stopwatch.tsx
+++ app/components/stopwatch.tsx
@@ -10,11 +10,11 @@ const Stopwatch = ({
 	const [running, setRunning] = useState(false);
 
 	useEffect(() => {
-		let interval;
+		let interval: any;
 		if (running) {
 			interval = setInterval(() => {
-				setTime((prevTime) => prevTime + 10);
-			}, 10);
+				setTime((prevTime) => prevTime + 1000);
+			}, 1000);
 		} else if (!running) {
 			clearInterval(interval);
 		}
@@ -45,11 +45,11 @@ const Stopwatch = ({
 					{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
 				</Text>
 				<Text style={{ fontSize: 32 }}>
-					{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:
+					{("0" + Math.floor((time / 1000) % 60)).slice(-2)}
 				</Text>
-				<Text style={{ fontSize: 32 }}>
+				{/* <Text style={{ fontSize: 32 }}>
 					{("0" + ((time / 10) % 100)).slice(-2)}
-				</Text>
+				</Text> */}
 			</View>
 			<View
 				style={{
