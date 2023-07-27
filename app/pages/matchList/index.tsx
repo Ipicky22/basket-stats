7b29b251262f79dc30055208e4dfa50db0689164
Adrien Masson <adrien.masson@hotmail.fr>
2023-07-27 21:07:45 +0200

    remove modal for screen - addMatch

--- app/pages/matchList/index.tsx
+++ app/pages/matchList/index.tsx
@@ -1,15 +1,16 @@
 import { View, ScrollView } from "react-native";
 import MatchList from "./matchList";
 import { Stack } from "expo-router";
-import ModalAddMatch from "./modalAddMatch";
+import HeaderRight from "./headerRight";
 
 const MatchListScreen = () => {
 	return (
 		<ScrollView style={{ flex: 1 }}>
 			<View style={{ padding: 12 }}>
-				<Stack.Screen options={{ title: "Match List" }} />
+				<Stack.Screen
+					options={{ title: "Match List", headerRight: HeaderRight }}
+				/>
 				<MatchList />
-				<ModalAddMatch />
 			</View>
 		</ScrollView>
 	);
